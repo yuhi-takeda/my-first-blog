4d8da434804f50b6c44a05c86127c52d0753e6db
YT <uralowl0505@gmail.com>
2020-07-25 00:09:08 +0900

    fix setting allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ov#l@g8oq7dc^3$^5cmv5%^0d$pf+j9n9v11_hz!#rw-%^lmdk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 's126yt.pythonanywhere.com']
 
 
 # Application definition
